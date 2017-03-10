be2913e7fb5fd4363a730b4074fba08f6cf57e76
Matthew Inman <Matthew.Inman@cdl.co.uk>
2017-03-10 14:00:06 +0000

    Initial commit with pom.xml settings for spring boot.

---
+++ src/main/java/uk/co/pped/springdatabaseservice/SpringDatabaseService.java
@@ -0,0 +1,11 @@
+package uk.co.pped.springdatabaseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class SpringDatabaseService extends SpringBootServletInitializer {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseService.class, args);
+	}
+}
